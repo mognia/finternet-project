5ad1def408bae654cf2c18807401cb32ba3b335a
Mognia <1mognia@gmail.com>
2023-01-23 16:36:43 +0330

    feature: adding footer button component features

--- src/app/components/footer-btns/footer-btns.component.ts
+++ src/app/components/footer-btns/footer-btns.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
 
 @Component({
   selector: 'app-footer-btns',
@@ -6,10 +6,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./footer-btns.component.scss']
 })
 export class FooterBtnsComponent implements OnInit {
-
+  @Output() nextPage  = new EventEmitter<boolean>();
+  @Output() prePage  = new EventEmitter<boolean>();
+  @Input() enablePre:boolean = true;
+  @Input() enableNext:boolean = true;
   constructor() { }
 
   ngOnInit(): void {
   }
-
+  onNext(value:boolean) {
+    this.nextPage.emit(value);
+  }
+  onPre(value:boolean) {
+    this.prePage.emit(value);
+  }
 }
